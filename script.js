8252dcafefa3fd0de8c27b1f6032308d563edcea
marvinjohn72019 <128127108+marvinjohn72019@users.noreply.github.com>
2023-03-30 10:23:19 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,26 @@
+fetch("products.json")
+.then(function(response){
+   return response.json();
+})
+.then(function(products){
+   let placeholder = document.querySelector("#data-output");
+   let out = "";
+   for(let product of products){
+      out +=` 
+      
+        <div class="product">
+               <img src="${item.image}" alt="${item.description}">
+               <p class="title">${item.title}</p>
+               <p class="description">${item.description}</p>
+               <p class="price">
+                  <span>${item.price}</span>
+                  <span>€</span>
+               </p>
+               <p class="cart">Add to cart <i class="bx bx-cart-alt"></i></p>
+            </div>
+         `;
+    }
+
+    placeholder.innerHTML = out;
+    
+  });
